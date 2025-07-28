7b9bef4695db7cc41967bb340df2c0e7093fa975
Dáire <daire@bura.games>
2025-07-28 16:51:33 +0100

    updated to template

---
+++ scripts/install.ps1
@@ -0,0 +1,27 @@
+# Installs wally packages and exports types
+
+# Remove old packages folders
+if (Test-Path -Path "Packages" -PathType Container) {
+    Remove-Item -Path "Packages" -Recurse -Force
+}
+if (Test-Path -Path "ServerPackages" -PathType Container) {
+    Remove-Item -Path "ServerPackages" -Recurse -Force
+}
+
+# Install packages
+wally install
+
+# If ServerPackages doesn't exist, create it
+if (-Not (Test-Path -Path "Packages" -PathType Container)) {
+    New-Item -Path "Packages" -ItemType Directory
+}
+if (-Not (Test-Path -Path "ServerPackages" -PathType Container)) {
+    New-Item -Path "ServerPackages" -ItemType Directory
+}
+
+# Sourcemap generation
+rojo sourcemap --output sourcemap.json default.project.json
+
+# Fix the types (why is this not native???)
+wally-package-types --sourcemap sourcemap.json Packages/
+wally-package-types --sourcemap sourcemap.json ServerPackages/
